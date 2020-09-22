4f34f6ce2a1a5ce0c9fbe0eb71a05a66df898425
Josh Bastean <jbastean@coker.edu>
2020-09-22 15:39:05 +0000

    Added connect-flash

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const flash = require('connect-flash');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
@@ -72,6 +73,9 @@ app.use(expressSession({
 
 // Method Override Config
 app.use(methodOverride('_method'));
+
+// Connect Flash
+app.use(flash());
 
 // Passport Config
 app.use(passport.initialize());
